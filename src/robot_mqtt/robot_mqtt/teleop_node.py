f0dae4b14ea7a9ada9e8bccb2f8cce1d9763b6f9
gdradag <gdradag@gmail.com>
2025-04-27 15:26:28 -0500

    Mejora en la comunicación

--- src/robot_mqtt/robot_mqtt/teleop_node.py
+++ src/robot_mqtt/robot_mqtt/teleop_node.py
@@ -2,14 +2,21 @@ import rclpy
 from rclpy.node import Node
 import paho.mqtt.client as mqtt
 from std_msgs.msg import String
+import threading
 
 class TeleopNode(Node):
     def __init__(self):
         super().__init__('teleop_node')
+
         # Configurar el cliente MQTT
         self.mqtt_client = mqtt.Client()
-        self.mqtt_client.connect("192.168.8.100", 1883, 60)  # Cambia "localhost" por la IP de tu broker MQTT
+        self.mqtt_client.connect("192.168.1.117", 1883, 60)  # Cambia "localhost" por la IP de tu broker MQTT
         self.get_logger().info("Teleop Node started. Listening to teleop_cmd topic.")
+
+        # Iniciar el loop MQTT en un hilo separado
+        mqtt_thread = threading.Thread(target=self.mqtt_loop)
+        mqtt_thread.daemon = True
+        mqtt_thread.start()
 
         # Suscriptor al tópico 'teleop_cmd'
         self.create_subscription(
@@ -18,6 +25,10 @@ class TeleopNode(Node):
             self.teleop_cmd_callback,
             10
         )
+
+    def mqtt_loop(self):
+        """Ejecutar el loop de MQTT en un hilo separado."""
+        self.mqtt_client.loop_start()
 
     def teleop_cmd_callback(self, msg):
         """Callback para recibir los comandos del nodo de interfaz y enviarlos por MQTT."""
@@ -40,4 +51,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
